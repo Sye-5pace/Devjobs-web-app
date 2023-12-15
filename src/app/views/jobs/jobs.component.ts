b92e63ba3d9a4dc9f9462c56ddd73f64ab20ec95
Sye-5pace <owusilasyp00@gmail.com>
2023-12-15 12:54:32 +0000

    :lipstick: Style/Chore: No-match UI created and data updated

--- src/app/views/jobs/jobs.component.ts
+++ src/app/views/jobs/jobs.component.ts
@@ -17,19 +17,15 @@ export class JobsComponent implements OnChanges {
   filterForm: FormGroup;
   titleValue: string = '';
   locationValue: string = '';
-  fulltimeValue: string = '';
-  parttimeValue: string = '';
   contractValue: string = '';
-  freelanceValue: string = '';
+  // notFound: boolean = false; 
+
 
   constructor(private fb: FormBuilder) {
     this.filterForm = this.fb.group({
       title: [''],
       location: [''],
-      fulltime: [false],
-      parttime: [false],
-      contract: [false],
-      freelance: [false]
+      contract: ['']
     });
   }
 
@@ -42,40 +38,35 @@ export class JobsComponent implements OnChanges {
 
 
   onSubmit(): void {
-    this.titleValue = this.filterForm.get('title')!.value.toLowerCase().trim();
-    this.locationValue = this.filterForm.get('location')!.value.toLowerCase();
-    this.fulltimeValue = this.filterForm.get('fulltime')!.value;
-    this.parttimeValue = this.filterForm.get('parttime')!.value;
-    this.contractValue = this.filterForm.get('contract')!.value;
-    this.freelanceValue = this.filterForm.get('freelance')!.value;
-
-    console.log('Filterform:', this.filterForm.value);
-
     this.applyFilters()
   }
-
+  
   applyFilters(): JobsData[] {
+    this.titleValue = this.filterForm.get('title')!.value.toLowerCase().trim();
+    this.locationValue = this.filterForm.get('location')!.value.toLowerCase();
+    this.contractValue = this.filterForm.get('contract')!.value.toLowerCase();
+    console.log('Filterform:', this.filterForm.value);
+
     this.filteredJobs = [...this.jobsData];
 
     if (this.titleValue) {
       this.filteredJobs = this.filteredJobs.filter(job =>
         job.position.toLowerCase().includes(this.titleValue)
       );
-      console.log('If titleValue:', this.filteredJobs);
     }
 
     if (this.locationValue) {
       this.filteredJobs = this.filteredJobs.filter(job =>
         job.location.toLowerCase().includes(this.locationValue)
       );
-      console.log('If locationValue:', this.filteredJobs);
     }
 
-    if (this.fulltimeValue) {
-      this.filteredJobs = this.filteredJobs.filter(job => job.contract);
-      console.log('If fulltimeValue:', this.filteredJobs);
+    if (this.contractValue) {
+      this.filteredJobs = this.filteredJobs.filter(job => 
+        job.contract.toLowerCase() === this.contractValue);
     }
 
+    // this.notFound = 
     return this.filteredJobs;
   }
 
